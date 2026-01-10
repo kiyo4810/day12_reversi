66dc2c9e03f489ece98b07fb648d5d2f3d523d06
kiyo4810 <kiyo4810@gmail.com>
2026-01-10 23:30:22 +0900

    1−b改造

--- chapter-1/1-b/js/main.js
+++ chapter-1/1-b/js/main.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#target').textContent = '文字列を出力';
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('#target').textContent = '文字列を出力';
+// });
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#target").textContent ="ララララブソングを歌おう！"
+    console.log("ログには隠しメッセージを入れよう");
+    
+}))
