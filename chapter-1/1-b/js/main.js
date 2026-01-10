903fb415ae5b4134dc44c99938585340daff8aac
kiyo4810 <kiyo4810@gmail.com>
2026-01-10 23:43:22 +0900

    getElementByIDに変えたバージョン

--- chapter-1/1-b/js/main.js
+++ chapter-1/1-b/js/main.js
@@ -1,7 +1,12 @@
 // document.addEventListener('DOMContentLoaded', function() {
 //     document.querySelector('#target').textContent = '文字列を出力';
 // });
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelector("#target").textContent = "ララララブソングを歌おう！";
+//   console.log("ログには隠しメッセージを入れよう");
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#target").textContent = "ララララブソングを歌おう！";
-  console.log("ログには隠しメッセージを入れよう");
+  document.getElementById("target").textContent = "getElementByIDに変えたバージョン";
+  console.log("getElementByIDに変えたバージョン");
 });
