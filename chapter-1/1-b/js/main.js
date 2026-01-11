c29d3f0812f4028a4af197304efb9c5ce9952e35
kiyo4810 <kiyo4810@gmail.com>
2026-01-11 12:41:59 +0900

    コンソールログ実験

--- chapter-1/1-b/js/main.js
+++ chapter-1/1-b/js/main.js
@@ -10,3 +10,4 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("target").textContent = "getElementByIDに変えたバージョン";
   console.log("getElementByIDに変えたバージョン");
 });
+console.log(123, "文字列", true);
