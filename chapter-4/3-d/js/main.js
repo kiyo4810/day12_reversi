a388e5a2bc4481c06ede60631dc7327885695d7d
kiyo4810 <kiyo4810@gmail.com>
2026-01-14 14:25:34 +0900

    八木ドナピマイルド追記

--- chapter-4/3-d/js/main.js
+++ chapter-4/3-d/js/main.js
@@ -10,6 +10,14 @@ document.addEventListener("DOMContentLoaded", async function () {
   cobj.context.fillStyle = "white";
   // 3. 文字を書く（内容, X座標, Y座標）
   cobj.context.fillText("サバンナ八木!", 150, 200);
+  // 1. 画像の「箱」を作る
+  const img = new Image();
+  img.src = "yagi.webp"; // 画像のパス
+  // 2. 読み込みが終わったら実行する
+  img.onload = () => {
+    // drawImage(画像, x, y, 横幅, 縦幅)
+    cobj.context.drawImage(img, 10, 10, 200, 200);
+  };
 
   await gameUtil.sleep(1000); // 1000ミリ秒
   cobj.context.clearRect(0, 0, cobj.w, cobj.h);
@@ -21,6 +29,14 @@ document.addEventListener("DOMContentLoaded", async function () {
   cobj.context.fillStyle = "white";
   // 3. 文字を書く（内容, X座標, Y座標）
   cobj.context.fillText("ドーナツ・ピーナツ", 350, 350);
+  // 1. 画像の「箱」を作る
+  const img2 = new Image();
+  img2.src = "donapi.jpg"; // 画像のパス
+  // 2. 読み込みが終わったら実行する
+  img2.onload = () => {
+    // drawImage(画像, x, y, 横幅, 縦幅)
+    cobj.context.drawImage(img2, 10, 10, 200, 200);
+  };
 
   await gameUtil.sleep(1000); // 1000ミリ秒
   cobj.context.clearRect(0, 0, cobj.w, cobj.h);
@@ -32,4 +48,12 @@ document.addEventListener("DOMContentLoaded", async function () {
   cobj.context.fillStyle = "white";
   // 3. 文字を書く（内容, X座標, Y座標）
   cobj.context.fillText("三浦マイルド", 550, 500);
-});
+  // 1. 画像の「箱」を作る
+  const img3 = new Image();
+  img3.src = "miuramild.avif"; // 画像のパス
+  // 2. 読み込みが終わったら実行する
+  img3.onload = () => {
+    // drawImage(画像, x, y, 横幅, 縦幅)
+    cobj.context.drawImage(img3, 10, 10, 200, 200);
+  };
+}); // ここが全体の「閉じカッコ」！
